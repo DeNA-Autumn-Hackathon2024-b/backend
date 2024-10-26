89a112028b8e8c90c66064690d78f2ce74f7f1cc
yuorei <108039575+yuorei@users.noreply.github.com>
2024-10-26 14:25:07 +0900

    echo

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/DeNA-Autumn-Hackathon2024-b/backend/controller"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+
+	e.GET("/users/:id", controller.GetUser)
+	e.Logger.Fatal(e.Start(":8080"))
 }
