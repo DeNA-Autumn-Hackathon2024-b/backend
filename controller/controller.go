8af6120f0405bfcda7b1e3809dc52ca3d287c579
sikigasa <km3t1114@yahoo.co.jp>
2024-10-26 21:38:32 +0900

    update:controller

--- controller/controller.go
+++ controller/controller.go
@@ -1,13 +1,18 @@
 package controller
 
-import "github.com/DeNA-Autumn-Hackathon2024-b/backend/infra"
+import (
+	sqlc "github.com/DeNA-Autumn-Hackathon2024-b/backend/db/sqlc_gen"
+	"github.com/DeNA-Autumn-Hackathon2024-b/backend/infra"
+)
 
 type Controller struct {
 	infra *infra.Infrastructure
+	db    *sqlc.Queries
 }
 
 func NewController(infra *infra.Infrastructure) *Controller {
 	return &Controller{
 		infra: infra,
+		db:    sqlc.New(infra.ConnectDB()),
 	}
 }
