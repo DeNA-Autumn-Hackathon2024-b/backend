7a255e58de44f0601c25171e2fc14e1b9f2055c4
sikigasa <km3t1114@yahoo.co.jp>
2024-10-26 17:52:08 +0900

    update: model

--- db/query/query.sql
+++ db/query/query.sql
@@ -1,2 +1,14 @@
 -- name: GetUser :one
-SELECT * FROM users WHERE id = $1 LIMIT 1;
+SELECT * FROM "user" WHERE id = $1 LIMIT 1;
+
+-- name: PostUser :one
+INSERT INTO "user" (id, name, icon_url, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *;
+
+-- name: GetCassette :one
+SELECT * FROM cassette WHERE id = $1 LIMIT 1;
+
+-- name: GetCassettesByUser :many
+SELECT * FROM cassette WHERE user_id = $1;
+
+-- name: PostCassette :one
+INSERT INTO cassette (id, user_id, name, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *;
