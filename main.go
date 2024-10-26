8568951232ce05ac433ee0a46b045dcd985c0126
sikigasa <km3t1114@yahoo.co.jp>
2024-10-26 21:39:07 +0900

    fix: main

--- main.go
+++ main.go
@@ -4,39 +4,22 @@ import (
 	"net/http"
 
 	"github.com/DeNA-Autumn-Hackathon2024-b/backend/controller"
-	sqlc "github.com/DeNA-Autumn-Hackathon2024-b/backend/db/sqlc_gen"
-	dbDriver "github.com/DeNA-Autumn-Hackathon2024-b/backend/infra"
 
 	"github.com/DeNA-Autumn-Hackathon2024-b/backend/infra"
 	"github.com/labstack/echo/v4"
-	echo "github.com/labstack/echo/v4"
 )
-
-type router struct {
-	echo *echo.Echo
-	sql  *sqlc.Queries
-}
 
 func main() {
 	e := echo.New()
-	db := dbDriver.ConnectDB()
-	sql := sqlc.New(db)
-
-	router := &router{
-		echo: e,
-		sql:  sql,
-	}
-
-	ca := controller.NewCassette(sql)
-	router.echo.GET("/cassettes", ca.GetCassette)
-	router.echo.GET("/health", func(c echo.Context) error {
-		return c.String(http.StatusOK, "OK")
-	})
 
 	i := infra.NewInfrastructure()
 	c := controller.NewController(i)
+	e.GET("/cassettes", c.GetCassettesByUser)
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
 
-	router.echo.GET("/users/:id", c.GetUser)
+	e.GET("/users/:id", c.GetUser)
 	e.POST("/song", c.UploadSong)
-	router.echo.Logger.Fatal(e.Start(":8086"))
+	e.Logger.Fatal(e.Start(":8086"))
 }
