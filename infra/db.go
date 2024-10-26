54e107dcc2617c9703f69faff96077c6c37dd451
sikigasa <km3t1114@yahoo.co.jp>
2024-10-26 23:33:00 +0900

    delete: func

--- infra/db.go
+++ infra/db.go
@@ -2,37 +2,11 @@ package infra
 
 import (
 	"context"
-	"database/sql"
 	"log"
 
 	"github.com/jackc/pgx/v5"
 	_ "github.com/lib/pq"
 )
-
-type Connection interface {
-	Connection() (*sql.DB, error)
-	// Close(ctx context.Context) error
-}
-
-func setupDB(dbDriver string, dsn string) (*sql.DB, error) {
-	db, err := sql.Open(dbDriver, dsn)
-	if err != nil {
-		return nil, err
-	}
-	return db, err
-}
-
-func Connect() *sql.DB {
-	dbDriver := "postgres"
-	dsn := "host=postgres_db_1 port=5432 user=postgres password=postgres dbname=cassette sslmode=disable"
-	db, err := setupDB(dbDriver, dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	return db
-}
 
 func (i *Infrastructure) ConnectDB() *pgx.Conn {
 	ctx := context.Background()
